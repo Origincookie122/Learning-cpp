f441faa57a94eee6d9352724acfc6a3c3dcf7c86
Origincookie122 <83477409+Origincookie122@users.noreply.github.com>
2025-05-14 16:38:54 -0400

    Learning Arrays and multidimensional arrays

---
+++ Section7/Arrays/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+	
+	// Regular array test
+//	const int highschoolers {350};
+//	int finalTestingScores [highschoolers] {0};
+//	
+//	for (int i {0}; i < highschoolers; i++) {
+//		int testScore = rand()%40+60;
+//		finalTestingScores[i] = testScore;
+//		std::cout << finalTestingScores[i] << std::endl;
+//	}
+
+//	Gonna make a movie rating system using multidimensional arrays(2D)
+//
+//	int movieRatings [1][5] {};
+//	
+//	std::cout << ("Reviewer 1, what is your ratings for the 5 movie you watched?: ");
+//	std::cin >> movieRatings[0][0];
+//	std::cin >> movieRatings[0][1];
+//	std::cin >> movieRatings[0][2];
+//	std::cin >> movieRatings[0][3];
+//	std::cin >> movieRatings[0][4];
+//	
+//	std::cout << movieRatings[0][0];
+	return 0;
+}
